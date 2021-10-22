e69666e7fa52e07441982a60e4cb7778ae3df1cb
naomifreitas <79206033+naomifreitas@users.noreply.github.com>
2021-10-22 11:50:41 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,40 @@
-function trocarcor(){
-    const texto = document.getElementById("intro")
+function trocarcor() {
+    const texto = document.getElementById("intro");
     texto.style.color = "blue";
 }
+
+function trocarcor() {
+    const texto = document.getElementById("intro");
+    texto.style.color = "none";
+}
+
+function trocarcor() {
+    const texto = document.getElementById("intro");
+    texto.style.color = "block";
+}
+/*
+  h1 {
+    color: black;
+    font-family: 'Satisfy', cursive;
+    text-align: center;
+    margin-top: 80px;
+    margin-bottom: 100px;
+    font-size: 50px;
+  }
+
+ h2{
+    color: black;
+    font-family: 'Satisfy';
+    text-align: left;
+    font-size: 25px;
+    padding-top: 10px;
+    padding-right: 30px;
+    padding-bottom: 40px;
+    padding-left: 30px;
+    margin-top: 30px;
+  }
+ h3 {
+      color: black;
+      font-family: 'Satisfy';
+      font-size: 20px;
+  }
